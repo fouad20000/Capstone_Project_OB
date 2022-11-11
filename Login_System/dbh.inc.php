ca0e7522e2e9d6cd54ba2a5e69d5f6b33d620941
fouad20000 <50175365+fouad20000@users.noreply.github.com>
2022-11-11 14:39:17 +0300

    Update dbh.inc.php

--- Login_System/dbh.inc.php
+++ Login_System/dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$serverName = "localhost";
-$DBusername = "root";
-$DBpassword = "";
-$DBname = "ObjectDetectionProject";
+$serverName = "us-cdbr-east-06.cleardb.net";
+$DBusername = "b34b2afa7c586b";
+$DBpassword = "06b73f57";
+$DBname = "heroku_b4ce68d5d2b1e41";
 
 $conn = mysqli_connect($serverName, $DBusername, $DBpassword, $DBname);
 
