083e533b972b8ee8147e3a3225e453b29e3031dc
fouad20000 <50175365+fouad20000@users.noreply.github.com>
2022-11-13 19:01:35 +0300

    DP column deleted

--- AdminDashboard/AccountTable.php
+++ AdminDashboard/AccountTable.php
@@ -79,7 +79,6 @@
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Tables:</h6>
                         <a class="collapse-item" href="LPI.php">License plate information</a>
-                        <a class="collapse-item" href="DP.php">Detected Plates</a>
                     </div>
                 </div>
             </li>
