6c0e62742db912dcba0b925db25c8d9ca29a09ca
fouad20000 <50175365+fouad20000@users.noreply.github.com>
2022-11-13 19:26:19 +0300

    Revert "Revert "DP.php and crud deleted""
    This reverts commit f1574da725d55c6b56b878b7813daa6588fd9fde.

--- AdminDashboard/AccountTable.php
+++ AdminDashboard/AccountTable.php
@@ -37,81 +37,60 @@
                     <i class="fas fa-laugh-wink"></i>
                 </div>
                 <div class="sidebar-brand-text mx-3">Back To Home Page</div>
+            </a>
 
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
 
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <a class="nav-link" href="Mainpage.php">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+            </li>
 
-                <!-- Divider -->
-                <hr class="sidebar-divider">
+            <!-- Divider -->
+            <hr class="sidebar-divider">
 
-                <!-- Heading -->
-                <div class="sidebar-heading">
-                    Table
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Table
+            </div>
+
+            <!-- Nav Item - Account Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="AccountTable.php" data-toggle="collapse" data-target="#collapseTwo"
+                    aria-expanded="true" aria-controls="collapseTwo">
+                    <i class="fas fa-fw fa-cog"></i>
+                    <span>Account</span>
+                </a>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingUtilities"
+                    data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Tables:</h6>
+                        <a class="collapse-item" href="AccountTable.php">Account Table</a>
+                    </div>
                 </div>
+            </li>
 
-                <!-- Nav Item - Account Menu -->
-                <li class="nav-item">
-                    <a class="nav-link collapsed" href="AccountTable.php" data-toggle="collapse"
-                        data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                        <i class="fas fa-fw fa-cog"></i>
-                        <span>Account</span>
-                    </a>
-                    <div id="collapseTwo" class="collapse" aria-labelledby="headingUtilities"
-                        data-parent="#accordionSidebar">
-                        <div class="bg-white py-2 collapse-inner rounded">
-                            <h6 class="collapse-header">Tables:</h6>
-                            <a class="collapse-item" href="AccountTable.php">Account Table</a>
-                        </div>
-                    </div>
-
-                    <<<<<<< HEAD <!-- Nav Item - License Plate Number Collapse Menu -->
-                <li class="nav-item">
-                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-                        aria-expanded="true" aria-controls="collapseUtilities">
-                        <i class="fas fa-fw fa-wrench"></i>
-                        <span>License Plate Number</span>
-                    </a>
-                    <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-                        data-parent="#accordionSidebar">
-                        <div class="bg-white py-2 collapse-inner rounded">
-                            <h6 class="collapse-header">Tables:</h6>
-                            <a class="collapse-item" href="LPI.php">License plate information</a>
-                            <<<<<<< HEAD <<<<<<< HEAD=======<!-- Nav Item - Account Menu -->
-                <li class="nav-item">
-                    <a class="nav-link collapsed" href="AccountTable.php" data-toggle="collapse"
-                        data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                        <i class="fas fa-fw fa-cog"></i>
-                        <span>Account</span>
-                    </a>
-                    <div id="collapseTwo" class="collapse" aria-labelledby="headingUtilities"
-                        data-parent="#accordionSidebar">
-                        <div class="bg-white py-2 collapse-inner rounded">
-                            <h6 class="collapse-header">Tables:</h6>
-                            <a class="collapse-item" href="AccountTable.php">Account Table</a>
-                        </div>
-
-                        <a class="collapse-item" href="DP.php">Detected Plates</a>
-
+            <!-- Nav Item - License Plate Number Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+                    aria-expanded="true" aria-controls="collapseUtilities">
+                    <i class="fas fa-fw fa-wrench"></i>
+                    <span>License Plate Number</span>
+                </a>
+                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+                    data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Tables:</h6>
+                        <a class="collapse-item" href="LPI.php">License plate information</a>
                         <a class="collapse-item" href="DP.php">Detected Plates</a>
                     </div>
-                </li>
-
-                <!-- Nav Item - License Plate Number Collapse Menu -->
-                <li class="nav-item">
-                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-                        aria-expanded="true" aria-controls="collapseUtilities">
-                        <i class="fas fa-fw fa-wrench"></i>
-                        <span>License Plate Number</span>
-                    </a>
-                    <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-                        data-parent="#accordionSidebar">
-                        <div class="bg-white py-2 collapse-inner rounded">
-                            <h6 class="collapse-header">Tables:</h6>
-                            <a class="collapse-item" href="LPI.php">License plate information</a>
-                        </div>
-                    </div>
-                </li>
-                <!-- Divider -->
-                <hr class="sidebar-divider d-none d-md-block">
+                </div>
+            </li>
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
 
         </ul>
         <!-- End of Sidebar -->
